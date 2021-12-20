58098f0ead090aa672d1e1075848e68b1958809f
Felipe Kenzo <felipekenzohamada@gmail.com>
2021-12-20 00:16:13 -0300

    add script to create symbolic link automatically

---
+++ make-symbolic.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+files=()
+
+files[0]=".bashrc"
+files[1]=".bash_profile"
+files[2]=".bash_aliases"
+files[3]=".bash_function"
+files[4]=".dircolors"
+
+i=0
+
+echo "Create dotfiles symbolink..."
+
+for file in ${files[@]}; do
+  ln -sv "$(pwd)/${file}" "${HOME}/${file}"
+  let i++
+done
+
+echo "Success!"
+
